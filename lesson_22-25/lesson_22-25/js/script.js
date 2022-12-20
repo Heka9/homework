eac2f15eccf3660fe9f4b08db33d538de5623130
Heka9 <bliznyukjenia93@gmail.com>
2022-12-20 22:20:43 +0200

    222

---
+++ lesson_22-25/lesson_22-25/js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log("Я учу JS");
+
+const USER_EYES_COLOR = "#964b00";
+console.log(USER_EYES_COLOR);
+
+let user, userName;
+userName = "Вася";
+user = userName;
+console.log(user);
+
